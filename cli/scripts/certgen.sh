8a3adb0fa7300329bcd3fc69dab3c6cfa97e1e1b
jacohend <jacob@tierion.com>
2019-05-21 17:00:38 -0700

    move tls generation / cert home to ~/.chainpoint/node

--- cli/scripts/certgen.sh
+++ cli/scripts/certgen.sh
@@ -24,7 +24,12 @@ PASSPHRASE=""
 # Filename of generated openssl config file
 CONFIG_FILE="certgen.cnf"
 
-cat > $CONFIG_FILE <<-EOF
+HOME=$(eval printf ~$USER)
+CONFIG_DIR=$HOME/.chainpoint/node
+echo $CONFIG_DIR
+mkdir -p $CONFIG_DIR
+
+cat > $CONFIG_DIR/$CONFIG_FILE <<-EOF
 [req]
 distinguished_name = req_distinguished_name
 x509_extensions = v3_req
@@ -61,18 +66,18 @@ FILE_NAME="cert"
 
 # Remove previous cert files
 [ -f ./cert.key ] && \
-  chmod 770 $FILE_NAME.* && \
-  rm $FILE_NAME.* && \
+  chmod 770 $CONFIG_DIR/$FILE_NAME.* && \
+  rm $CONFIG_DIR/$FILE_NAME.* && \
   echo 'Old TLS cert files removed' || true
 
 echo "Generating certificate for IP Address : $IP_ADDRESS"
 
 # Generate our Private Key, CSR and Certificate
 # Use SHA-2 as SHA-1 is unsupported from Jan 1, 2017
-openssl req -new -x509 -newkey rsa:4096 -sha256 -nodes -keyout "$FILE_NAME.key" -days $DAYS -out "$FILE_NAME.crt" -passin pass:$PASSPHRASE -config "$CONFIG_FILE"
+openssl req -new -x509 -newkey rsa:4096 -sha256 -nodes -keyout "$CONFIG_DIR/$FILE_NAME.key" -days $DAYS -out "$CONFIG_DIR/$FILE_NAME.crt" -passin pass:$PASSPHRASE -config "$CONFIG_DIR/$CONFIG_FILE"
 
 # Store the human readable details of the generated crt in *.info file
-openssl x509 -noout -fingerprint -text < "$FILE_NAME.crt" > "$FILE_NAME.info"
+openssl x509 -noout -fingerprint -text < "$CONFIG_DIR/$FILE_NAME.crt" > "$CONFIG_DIR/$FILE_NAME.info"
 
 # Protect the key
-chmod 444 "$FILE_NAME.key"
+chmod 444 "$CONFIG_DIR/$FILE_NAME.key"
